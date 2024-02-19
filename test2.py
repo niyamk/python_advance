326a237885859d6e3bc300f948b661154556ab2a
niyamk <88551330+niyamk@users.noreply.github.com>
2024-02-19 21:56:08 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,5 +3,5 @@ bot = telepot.Bot('6478005736:AAHJ3R4gpxhoyKBocG1GixN4OvkMpup-DHQ')
 bot.sendMessage(6438509949,'yo')
 # a = bot.
 
-print(a)
+# print(a)
 # bot.sendAudio(6438509949,open('D:\clg_folder\himanshu.mp3','r'), title="Audio Title", caption="Optional caption")
